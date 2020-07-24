22b8399148d0e32c8c1d47bf0cea77182db63c5f
Detonation <8228643+detonation@users.noreply.github.com>
2020-07-24 00:34:40 -0500

    Add files via upload
    Cleaned up comments, timing adjustment for more responsive feel.

--- startstop.ino
+++ startstop.ino
@@ -7,6 +7,9 @@ START DOUBLE CLICK: TRACK NEXT
 STOP SINGLE PRESS: STOP
 STOP LONG PRESS: VOLUME DOWN
 STOP DOUBLE CLICK: TRACK PREVIOUS
+
+Many thanks to the Creators of OneButton Library and HID-Project library.
+Their hard work made this project a breeze.
 */
 
 #include "OneButton.h"
@@ -18,45 +21,45 @@ OneButton button1(3, true); // works perfectly NO
 OneButton button2(2, false, true); //works perfectly NC
 const int pinLed = LED_BUILTIN;
 
-
 // setup code here, to run once:
 void setup() {
   Consumer.begin(); //start the HID process
-//  Serial.begin(9600);
+//  Serial.begin(9600); //no need for serial, was just for troubleshooting
 //  while (!Serial) {
 //    ;
 //  }
 //  Serial.println("Starting TwoButtons...");
 
-  // link the button 1 functions.
+//lowering threshold for double-click from 600 to 250ms 
+  button1.setClickTicks(500);
+
+//lowering threshold for long-press from 1000 to 500ms
+  button1.setPressTicks(500);
+
+//doing the same for the other button
+  button2.setClickTicks(500);
+  button2.setPressTicks(500);
+  
+// link the button 1 functions.
   button1.attachClick(click1);
   button1.attachDoubleClick(doubleclick1);
   button1.attachLongPressStart(longPressStart1);
   button1.attachLongPressStop(longPressStop1);
   button1.attachDuringLongPress(longPress1);
 
-  // link the button 2 functions.
+ // link the button 2 functions.
   button2.attachClick(click2);
   button2.attachDoubleClick(doubleclick2);
   button2.attachLongPressStart(longPressStart2);
   button2.attachLongPressStop(longPressStop2);
   button2.attachDuringLongPress(longPress2);
 
-} // setup
-
-
-// main code here, to run repeatedly: 
+}
 void loop() {
-  // keep watching the push buttons:
   button1.tick();
   button2.tick();
-
-  // You can implement other code in here or just wait a while 
   delay(10);
-} // loop
-
-
-// ----- button 1 callback functions
+}
 
 // This function will be called when the button1 was pressed 1 time (and no 2. button press followed).
 void click1() {
@@ -64,20 +67,16 @@ void click1() {
 Consumer.write(MEDIA_PLAY_PAUSE);
 } // click1
 
-
 // This function will be called when the button1 was pressed 2 times in a short timeframe.
 void doubleclick1() {
-  Consumer.write(MEDIA_NEXT);
-
+Consumer.write(MEDIA_NEXT);
 //  Serial.println("Button 1 doubleclick.");
 } // doubleclick1
-
 
 // This function will be called once, when the button1 is pressed for a long time.
 void longPressStart1() {
 //  Serial.println("Button 1 longPress start");
 } // longPressStart1
-
 
 // This function will be called often, while the button1 is pressed for a long time.
 void longPress1() {
@@ -86,12 +85,10 @@ Consumer.write(MEDIA_VOLUME_UP);
 delay(75);
 } // longPress1
 
-
 // This function will be called once, when the button1 is released after beeing pressed for a long time.
 void longPressStop1() {
 //  Serial.println("Button 1 longPress stop");
 } // longPressStop1
-
 
 // ... and the same for button 2:
 
@@ -100,18 +97,14 @@ void click2() {
 Consumer.write(MEDIA_STOP);
 } // click2
 
-
 void doubleclick2() {
 //  Serial.println("Button 2 doubleclick.");
 Consumer.write(MEDIA_PREVIOUS);
-
 } // doubleclick2
-
 
 void longPressStart2() {
 //  Serial.println("Button 2 longPress start");
 } // longPressStart2
-
 
 void longPress2() {
 //Serial.println("Button 2 longPress...");
@@ -122,6 +115,3 @@ delay(75);
 void longPressStop2() {
 //  Serial.println("Button 2 longPress stop");
 } // longPressStop2
-
-
-// End
